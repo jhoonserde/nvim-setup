b4838d9b13ac7580b56cab776c6d32a3511ecefd
jhoonserde <reyhanramadhan9928@gmail.com>
2026-06-21 16:36:20 +0700

    set preview window to right

--- lua/plugins/oilnvim.lua
+++ lua/plugins/oilnvim.lua
@@ -5,23 +5,21 @@ return {
 	opts = {
 		default_file_explorer = true,
 		keymaps = {
-			["e?"] = { "actions.show_help", mode = "n" },
-			["<CR>"] = "actions.select",
 			["<C-t>"] = false,
 			["<C-h>"] = false,
-			["<C-s>"] = { "actions.select", opts = { tab = true } },
-			["<C-p>"] = "actions.preview",
-			["<C-c>"] = { "actions.close", mode = "n" },
 			["<C-r>"] = "actions.refresh",
-			["-"] = { "actions.parent", mode = "n" },
-			["_"] = { "actions.open_cwd", mode = "n" },
-			["`"] = { "actions.cd", mode = "n" },
-			["e~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
-			["es"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
-			["ex"] = "actions.open_external",
-			["e."] = { "actions.toggle_hidden", mode = "n" },
-			["e\\"] = { "actions.toggle_trash", mode = "n" },
+			["<C-p>"] = {
+				callback = function()
+					local oil = require("oil")
+					oil.open_preview({ vertical = true, split = "botright" }, function(err)
+						if not err then
+							vim.api.nvim_command("vertical resize 30")
+						end
+					end)
+				end,
+			},
 		},
+		float = { preview_split = "right" },
 	},
 	keys = {
 		{ "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
